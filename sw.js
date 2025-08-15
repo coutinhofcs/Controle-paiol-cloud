8965a409c8534e15336bad5d2e98fcf47a51d46d
coutinhofcs <f.v.trabalhos@gmail.com>
2025-08-15 07:23:41 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open('paiol-v2').then(c=>c.addAll(['.','index.html','manifest.webmanifest'])));
+});
+self.addEventListener('fetch', e=>{ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });
